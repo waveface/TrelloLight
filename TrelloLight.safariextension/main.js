ad1d960be44bead2085840009a75870f20a42d64
Jamie Sa <james.sa.2013@gmail.com>
2011-10-27 11:50:12 +0800

    exclude backlog and trigger more correctly on start up

--- TrelloLight.safariextension/main.js
+++ TrelloLight.safariextension/main.js
@@ -1,15 +1,17 @@
-//BUG: Trello didn't at first load, need a refresh to make it work
+$().ready(function() {
+	setTimeout(ScrumColorize, 2000);
+});
 
-setTimeout(ScrumColorize, 2000);
-
-function CountHours(root){
+function CountHours(root, color){
+	if( color === undefined ) {
+		color = '#ff284a'
+	}
 	var total = 0.0;
 	$(root).find('h3.list-card-title a').each(function(){
 		var estimate = $(this).text().match(/\((\d*\.?\d*)H?\)/);
 		console.log($(this).text());
-		console.log(estimate);
 		if( estimate == null ){
-			$(this).css('color','#ff284a');
+			$(this).css('color', color);
 		}else {
 			total += parseFloat(estimate[1]);
 		}
@@ -20,22 +22,27 @@ function CountHours(root){
 function ScrumColorize(){
 
 var total = 0.0;
-var done = 0.0;
-
 
 total = CountHours($('div.list div.list-wrapper'));
 
-// Done
+var done = 0.0;
 $('div.list div.list-wrapper').each( function(){
-	//console.log($(this).text().indexOf('Done'));
-	if( $(this).find('h2').text().indexOf('Done') >= 0 ){
+	if( $(this).find('h2').text().toLowerCase().indexOf('done') >= 0 ){
 		done = CountHours($(this));
 	}
 })
 
-console.log(total + ' ' + done);
-remaining = Math.round(total*10-done*10)/10;
+var exclude = 0.0;
+$('div.list div.list-wrapper').each( function(){
+	if( $(this).find('h2').text().toLowerCase().indexOf('backlog') >= 0 ){
+		exclude = CountHours($(this), '#222');
+	}
+})
+
+console.log(total + ', ' + done + ', ' + exclude);
+total -= exclude;
 total = Math.round(total*10)/10;
+remaining = Math.round(total*10-done*10)/10;
 $('.board-title h2').text( $('.board-title h2').text() + ': ' + remaining + '/' + total);
 };
 
